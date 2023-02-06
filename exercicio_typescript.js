9399c2ceecc9a22e7faebdb0d229de49f105230e
Paulo César <pcgms7@hotmail.com>
2023-02-06 20:07:06 -0300

    exercicio TypeScript

---
+++ exercicio_typescript.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+function concatenacaoString(nome) {
+    return "ola" + nome;
+}
---
+++ exercicio_typescript.ts
@@ -0,0 +1,7 @@
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+function concatenacaoString(nome:string): string {
+    return "ola" + nome;
+}
